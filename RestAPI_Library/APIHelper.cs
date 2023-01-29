f484b8115d512545576c7304b1dabe18fc69ebef
Pepega2005 <75209223+Pepega2005@users.noreply.github.com>
2023-01-29 20:03:57 +0200

    added two projects

---
+++ RestAPI_Library/APIHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestAPI_Library
+{
+    public static class APIHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void Init()
+        {
+            ApiClient = new HttpClient();
+
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")
+            );
+        }
+    }
+}
